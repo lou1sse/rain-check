3bd2dd969a797b97ea54774949645e1a413a7c22
Louisse <louisseevale@icloud.com>
2024-06-17 19:41:42 +0800

    feat(MainPage): add functionality for current forecast and set up responsive ui

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,17 +1,50 @@
+import { useEffect, useState } from "react"
+import { CurrentForecastData, useForecast } from "../api"
 import {
   CurrentForecastComponent,
   HourlyForecastComponent,
-  SearchComponent
+  SearchComponent,
+  WeekForecastComponent
 } from "../components"
+import { DropdownComponent } from "../components/common"
 import styles from "./scss/styles.module.scss"
 
 function MainPage() {
+  const [keyword, setKeyword] = useState<string>("")
+  const [currentForecast, setCurrentForecast] =
+    useState<CurrentForecastData>()
+
+  const { currentForecastData, currentForecastRequest } = useForecast(
+    keyword || "Manila"
+  )
+
+  useEffect(() => {
+    if (!currentForecastData) {
+      currentForecastRequest()
+    }
+
+    setCurrentForecast(currentForecastData)
+  }, [currentForecastData])
+
   return (
     <div className={styles.mainPage}>
-      <div className={styles.mainPage__hero}>
-        <SearchComponent />
-        <CurrentForecastComponent />
-        <HourlyForecastComponent />
+      <div className={styles.mainPage__content}>
+        <div className={styles.hero}>
+          <div className={styles.hero__header}>
+            <SearchComponent
+              value={keyword}
+              onChangeValue={setKeyword}
+              onClickSearch={currentForecastRequest}
+            />
+            <DropdownComponent
+              value="Celsius"
+              options={["Celsius", "Fahrenheit"]}
+            />
+          </div>
+          <CurrentForecastComponent data={currentForecast} />
+          <HourlyForecastComponent />
+        </div>
+        <WeekForecastComponent />
       </div>
     </div>
   )
