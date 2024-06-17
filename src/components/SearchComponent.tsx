3f4edb98c373a0ebf818ef841554f75aa9879a83
Louisse <louisseevale@icloud.com>
2024-06-17 19:49:26 +0800

    feat(SearchComponent): add functionality

--- src/components/SearchComponent.tsx
+++ src/components/SearchComponent.tsx
@@ -1,11 +1,36 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline"
+import { ChangeEvent } from "react"
 import styles from "./scss/styles.module.scss"
 
-export function SearchComponent() {
+type Props = {
+  value: string,
+  onChangeValue?: (text: string) => void,
+  onClickSearch: () => void
+}
+
+export function SearchComponent(props: Props) {
+  const { value, onChangeValue, onClickSearch } = props
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value: inputValue } = event.target
+    if (onChangeValue) onChangeValue(inputValue)
+  }
+
   return (
     <div className={styles.searchComponent}>
-      <input type="text" placeholder="Search location" />
-      <MagnifyingGlassIcon />
+      <input
+        type="text"
+        value={value}
+        placeholder="Search location"
+        onChange={handleChange}
+      />
+      <button
+        type="button"
+        aria-label="Search Icon Button"
+        onClick={onClickSearch}
+      >
+        <MagnifyingGlassIcon />
+      </button>
     </div>
   )
 }
