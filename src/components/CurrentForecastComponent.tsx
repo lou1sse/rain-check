1dfd7696775ef1637a4075df26d414f00ba7ab22
Louisse <louisseevale@icloud.com>
2024-06-17 19:45:57 +0800

    feat(CurrentForecastComponent): create prop to display data from the api call and fix responsive ui

--- src/components/CurrentForecastComponent.tsx
+++ src/components/CurrentForecastComponent.tsx
@@ -1,15 +1,21 @@
+import { map } from "lodash-es"
+import { CurrentForecastData } from "../api"
 import styles from "./scss/styles.module.scss"
 
-export function CurrentForecastComponent() {
+type Props = { data: CurrentForecastData | undefined }
+
+export function CurrentForecastComponent(props: Props) {
+  const { data } = props
+
+  const renderWeather = () => map(data?.weather, "main").join(", ")
+
   return (
     <div className={styles.currentForecastComponent}>
-      <div className="flex justify-between w-full h-full">
-        <div className="flex flex-col justify-between h-full">
-          <p className="text-4xl font-medium">Manila</p>
-          <p className="text-7xl font-semibold">31</p>
-        </div>
-        <p>Sunny</p>
+      <div className="flex flex-col gap-y-12 justify-between h-full">
+        <p className="text-4xl font-medium">{data?.name}</p>
+        <p className="text-7xl font-semibold">{data?.main.temp}</p>
       </div>
+      <p>{renderWeather()}</p>
     </div>
   )
 }
